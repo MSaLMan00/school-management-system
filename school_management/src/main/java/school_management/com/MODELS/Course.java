aa167b111b2d8ff189f21d1375d5c7e75921c6d4
M Salman Khan <salmank2017@namal.edu.pk>
2025-06-25 03:16:05 +0500

    update formating

--- school_management/src/main/java/school_management/com/MODELS/Course.java
+++ school_management/src/main/java/school_management/com/MODELS/Course.java
@@ -1,72 +1,78 @@
 package school_management.com.MODELS;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.*;
-
 import java.util.HashSet;
 import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 public class Course {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @NotBlank(message = "Course name is required")
-    @Size(min = 2, max = 100)
-    private String name;
+  @NotBlank(message = "Course name is required")
+  @Size(min = 2, max = 100)
+  private String name;
 
-    private String description;
+  private String description;
 
-    @ManyToMany(mappedBy = "courses")
-    private Set<Student> students = new HashSet<>();
+  @ManyToMany(mappedBy = "courses")
+  private Set<Student> students = new HashSet<>();
 
-    @OneToMany(mappedBy = "course", cascade = CascadeType.ALL, orphanRemoval = true)
-    private Set<Grade> grades = new HashSet<>();
+  @OneToMany(mappedBy = "course", cascade = CascadeType.ALL, orphanRemoval = true)
+  private Set<Grade> grades = new HashSet<>();
 
-    public Long getId() {
-        return id;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public String getDescription() {
-        return description;
-    }
+  public String getDescription() {
+    return description;
+  }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+  public void setDescription(String description) {
+    this.description = description;
+  }
 
-    public Set<Student> getStudents() {
-        return students;
-    }
+  public Set<Student> getStudents() {
+    return students;
+  }
 
-    public void setStudents(Set<Student> students) {
-        this.students = students;
-    }
+  public void setStudents(Set<Student> students) {
+    this.students = students;
+  }
 
-    public Set<Grade> getGrades() {
-        return grades;
-    }
+  public Set<Grade> getGrades() {
+    return grades;
+  }
 
-    public void setGrades(Set<Grade> grades) {
-        this.grades = grades;
-    }
+  public void setGrades(Set<Grade> grades) {
+    this.grades = grades;
+  }
 
 }
